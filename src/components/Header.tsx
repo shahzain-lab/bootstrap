a3b06fd1848c6e4b295b331045d6df5c44154b98
shahzain <69709547+shahzain-lab@users.noreply.github.com>
2021-01-18 10:42:41 -0800

    setup navbar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,33 @@
 import React from 'react'
+import { Navbar,Nav, NavDropdown } from 'react-bootstrap';
 
 export const Header = () => {
     return (
         <div>
-          <nav className="navbar navbar-expand-md bg-dark navbar-dark">
-              <a className="navbar-brand text-warning font-weight-bold" href='#'>
-                  Newbie
-              </a>
-              <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar">
-                  <span className="navbar-toggler-icon"></span>
-              </button>
-              </nav>       
+            <nav className="bg-dark">
+            <div className="container">
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+  <Navbar.Brand href="#home">Newbie</Navbar.Brand>
+  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  <Navbar.Collapse id="responsive-navbar-nav">
+    <Nav className="ml-auto text-center">
+      <Nav.Link href="#features">HOME</Nav.Link>
+      <Nav.Link href="#features">ABOUT</Nav.Link>
+      <Nav.Link href="#pricing">PRICES</Nav.Link>
+      <NavDropdown title="PORTFOLIO" id="collasible-nav-dropdown">
+        <NavDropdown.Item  className="bg-dark text-white" href="#action/3.1">Web development</NavDropdown.Item>
+        <NavDropdown.Item  className="bg-dark text-white" href="#action/3.2">Native APP</NavDropdown.Item>
+        <NavDropdown.Item  className="bg-dark text-white" href="#action/3.3">UI-Designer</NavDropdown.Item>
+        <NavDropdown.Divider />
+        <NavDropdown.Item  className="bg-dark text-white" href="#action/3.4">MySelf</NavDropdown.Item>
+      </NavDropdown>
+      <Nav.Link href="#features">CONTACT</Nav.Link>
+    </Nav>
+   
+  </Navbar.Collapse>
+</Navbar> 
+</div>
+</nav>
         </div>
     )
 }
