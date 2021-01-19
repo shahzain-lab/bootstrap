37cb6ca71c70b4bdfbd4841890b20f1b94201c85
shahzain <69709547+shahzain-lab@users.noreply.github.com>
2021-01-19 12:24:57 -0800

    updated footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,6 +9,26 @@ export const Footer =()=> {
                     <div className="col-lg-4 col-md-4 col-sm-4 col-10">
                         
                     </div>
+                    <div className="col-lg-4 col-md-4 col-sm-4 col-10">
+                       <h2>ABOUT</h2>
+                        <ul>
+                            <li>Home</li>
+                            <li>About</li>
+                            <li>Services</li>
+                            <li>Contact</li>
+                            <li>Github</li>
+                            <li>Facebook</li>
+                            <li>Linkden</li>
+                        </ul>
+                    </div>
+                    <div className="col-lg-4 col-md-4 col-sm-4 col-10 text-center footer3">
+                        <h3>Subscribe to get latest updates</h3>
+                        <input 
+                          type="text"
+                          placeholder="Enter your Email"
+                        />
+                        <button className="btn btn-warning">Subscribe</button>
+                    </div>
                 </div>
             </div>
         </div>
