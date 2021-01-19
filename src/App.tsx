2b4cc0eb609c39b9797ad84797b391fafe4eeb23
shahzain <69709547+shahzain-lab@users.noreply.github.com>
2021-01-19 11:30:36 -0800

    our team section

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Header } from './components/Header';
 import './App.css'
 import { Services } from './components/Services';
 import { Portfolio } from './components/Portfolio';
+import { Team } from './components/Team';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
      <Header />
      <Services />
      <Portfolio />
+     <Team />
     </div>
   );
 }
---
+++ src/components/Team.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+export const Team =()=> {
+    return(
+        <div className="team bg-light">
+            <div className="container text-center">
+                <h2>Our Team</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                <div className="row">
+                    <div className="col-lg-4 col-md-4 col-sm-4 col-10 m-auto d-block">
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
