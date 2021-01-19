2854b06f7bf214ac2b7f523a2d6f234fe1f5e71f
shahzain <69709547+shahzain-lab@users.noreply.github.com>
2021-01-19 10:02:54 -0800

    card created

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import { Header } from './components/Header';
 import './App.css'
 import { Services } from './components/Services';
+import { Portfolio } from './components/Portfolio';
 
 function App() {
   return (
     <div>
      <Header />
      <Services />
+     <Portfolio />
     </div>
   );
 }
--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -1,13 +1,69 @@
 import React from 'react';
+import portfolio1 from '../img/portfolio1.jpg'
 
 export const Portfolio =()=> {
     return(
         <div className="portfolio bg-light">
-        <div className="container">
+        <div className="container text-center">
            <h2>Portfolio</h2>
            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-           <div className="">
-
+           <div className="row">
+             <div className="col-lg-4 col-md-4 col-sm-12 d-block m-auto col-10">
+                 <div className="card">
+                     <img src={portfolio1} alt="" className="img-fluid card-img" />
+                     <div className="card-body">
+                         <h2 className="card-title">Thread</h2>
+                         <p className="card-text">illustration</p>
+                     </div>
+                 </div>
+             </div>
+             <div className="col-lg-4 col-md-4 col-sm-12 d-block m-auto col-10">
+                 <div className="card">
+                     <img src={portfolio1} alt="" className="img-fluid card-img" />
+                     <div className="card-body">
+                         <h2 className="card-title">Thread</h2>
+                         <p className="card-text">illustration</p>
+                     </div>
+                 </div>
+             </div>
+             <div className="col-lg-4 col-md-4 col-sm-12 d-block m-auto col-10">
+                 <div className="card">
+                     <img src={portfolio1} alt="" className="img-fluid card-img" />
+                     <div className="card-body">
+                         <h2 className="card-title">Thread</h2>
+                         <p className="card-text">illustration</p>
+                     </div>
+                 </div>
+             </div>
+           </div>
+           <div className="row">
+             <div className="col-lg-4 col-md-4 col-sm-12 d-block m-auto col-10">
+                 <div className="card">
+                     <img src={portfolio1} alt="" className="img-fluid card-img" />
+                     <div className="card-body">
+                         <h2 className="card-title">Thread</h2>
+                         <p className="card-text">illustration</p>
+                     </div>
+                 </div>
+             </div>
+             <div className="col-lg-4 col-md-4 col-sm-12 d-block m-auto col-10">
+                 <div className="card">
+                     <img src={portfolio1} alt="" className="img-fluid card-img" />
+                     <div className="card-body">
+                         <h2 className="card-title">Thread</h2>
+                         <p className="card-text">illustration</p>
+                     </div>
+                 </div>
+             </div>
+             <div className="col-lg-4 col-md-4 col-sm-12 d-block m-auto col-10">
+                 <div className="card">
+                     <img src={portfolio1} alt="" className="img-fluid card-img" />
+                     <div className="card-body">
+                         <h2 className="card-title">Thread</h2>
+                         <p className="card-text">illustration</p>
+                     </div>
+                 </div>
+             </div>
            </div>
         </div>
         </div>
