74eea0bd6e3e0518ab753bdb124b418b1e660f67
shahzain <69709547+shahzain-lab@users.noreply.github.com>
2021-01-19 07:51:02 -0800

    create service

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import { Header } from './components/Header';
 import './App.css'
+import { Services } from './components/Services';
 
 function App() {
   return (
     <div>
      <Header />
+     <Services />
     </div>
   );
 }
---
+++ src/components/Services.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export const Services =()=> {
+    return(
+        <div className="container service text-center">
+            <h2>Services</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        <div className="row">
+            <div className="container col-lg-4 col-md-4 col-sm-4 col-10 d-block m-auto">
+                 <div className="imgSet m-auto d-block">
+                     <i className="fa fa-shopping-cart fa-3x"></i>
+                     <h2>E-commerece</h2>
+                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+                 </div>
+            </div>
+        </div>
+        </div>
+    )
+}
