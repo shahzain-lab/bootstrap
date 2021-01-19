3072ffbe3ec0f0c25a47c4dcb29b13722e74ee9d
shahzain <69709547+shahzain-lab@users.noreply.github.com>
2021-01-19 09:20:04 -0800

    portfolio

---
+++ src/components/Portfolio.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export const Portfolio =()=> {
+    return(
+        <div className="portfolio bg-light">
+        <div className="container">
+           <h2>Portfolio</h2>
+           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+           <div className="">
+
+           </div>
+        </div>
+        </div>
+    )
+}
