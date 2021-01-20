b4abae2f6f3dcf473245dd5e2aa5e1108721e708
shahzain <69709547+shahzain-lab@users.noreply.github.com>
2021-01-20 09:58:50 -0800

    responsive

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import { Footer } from './components/Footer';
 
 function App() {
   return (
-    <div>
+    <div className="app">
      <Header />
      <Services />
      <Portfolio />
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,24 +4,24 @@ import { Navbar,Nav, NavDropdown } from 'react-bootstrap';
 export const Header = () => {
     return (
         <div className="header">
-            <nav className="bg-dark">
+            <nav className="navsec bg-dark">
             <div className="container">
-        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Navbar collapseOnSelect expand="lg" variant="dark">
   <Navbar.Brand href="#home"className="text-warning">Newbie</Navbar.Brand>
   <Navbar.Toggle aria-controls="responsive-navbar-nav" />
   <Navbar.Collapse id="responsive-navbar-nav">
     <Nav className="ml-auto text-center">
-      <Nav.Link href="#features">HOME</Nav.Link>
-      <Nav.Link href="#features">ABOUT</Nav.Link>
-      <Nav.Link href="#pricing">PRICES</Nav.Link>
-      <NavDropdown title="PORTFOLIO" id="collasible-nav-dropdown">
+      <Nav.Link href="#features" className="text-white">HOME</Nav.Link>
+      <Nav.Link href="#features"className="text-white">ABOUT</Nav.Link>
+      <Nav.Link href="#pricing"className="text-white">PRICES</Nav.Link>
+      <NavDropdown className="port" title="PORTFOLIO" id="collasible-nav-dropdown">
         <NavDropdown.Item  className="bg-dark text-white" href="#action/3.1">Web development</NavDropdown.Item>
         <NavDropdown.Item  className="bg-dark text-white" href="#action/3.2">Native APP</NavDropdown.Item>
         <NavDropdown.Item  className="bg-dark text-white" href="#action/3.3">UI-Designer</NavDropdown.Item>
         <NavDropdown.Divider />
         <NavDropdown.Item  className="bg-dark text-white" href="#action/3.4">MySelf</NavDropdown.Item>
       </NavDropdown>
-      <Nav.Link href="#features">CONTACT</Nav.Link>
+      <Nav.Link href="#features" className="text-white">CONTACT</Nav.Link>
     </Nav>
    
          </Navbar.Collapse>
@@ -31,7 +31,7 @@ export const Header = () => {
          <div className="container text-center head">
            <h2>Welcome to our studio</h2>
            <h1>Pick whats you want</h1>
-           <button className="btn btn-warning text-white btn-lg font-weight-bold">Subscribe</button>
+           <button className="btn btn-warning text-white main-btn btn-lg font-weight-bold">Subscribe</button>
          </div>
         </div>
     )
