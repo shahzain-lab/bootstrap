2c9727448f70ecacaf127ef5bc6718dc821611a2
shahzain <69709547+shahzain-lab@users.noreply.github.com>
2021-01-20 08:24:14 -0800

    footer content added

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,34 +3,52 @@ import React from 'react';
 
 export const Footer =()=> {
     return(
-        <div className="footer">
-            <div className="container">
-                <div className="row">
-                    <div className="col-lg-4 col-md-4 col-sm-4 col-10">
-                        
-                    </div>
-                    <div className="col-lg-4 col-md-4 col-sm-4 col-10">
-                       <h2>ABOUT</h2>
-                        <ul>
-                            <li>Home</li>
-                            <li>About</li>
-                            <li>Services</li>
-                            <li>Contact</li>
-                            <li>Github</li>
-                            <li>Facebook</li>
-                            <li>Linkden</li>
-                        </ul>
-                    </div>
-                    <div className="col-lg-4 col-md-4 col-sm-4 col-10 text-center footer3">
-                        <h3>Subscribe to get latest updates</h3>
-                        <input 
-                          type="text"
-                          placeholder="Enter your Email"
-                        />
-                        <button className="btn btn-warning">Subscribe</button>
-                    </div>
-                </div>
+        <footer className="footersection "id="footerid">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-4 col-md-6 col-12 footerdiv text-uppercase text-white">
+              <h3>about our team</h3>
+              <li>Our Company have 5 year expereince</li>
+              <li> in digital marketing and also</li>
+              <li> everyday deal with high Qualifier people.</li>
+              <li>We support our team with bonus packages.</li>
+              
             </div>
-        </div>
+            <div className="col-lg-4 col-md-6 col-12 footerdiv text-center text-uppercase">
+              <h3>navigation links</h3>
+              <li><a href="#">Home</a></li>
+              <li><a href="#servicediv">services</a></li>
+              <li><a href="#footerid">about</a></li>
+              <li><a href="#pricingdiv">prices</a></li>
+            </div>
+            <div className="col-lg-4 col-md-6 col-12 footerdiv text-uppercase">
+              <h3>news letter</h3>
+              <p>If you have any doubt please email us and also subscribe our newsletter for daily updates
+
+              </p>
+              <div className="container mainsub">
+                <div className="row">
+                  <div className="col-lg-12 col-12">
+                    <div className="input-group mb-3">
+                      <input type="text" className="form-control news" placeholder="Your Email" />
+                      <div className="input-group-append">
+                        <span className="input-group-text">Subscribe</span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+            </div>
+          </div>
+
+
+          <div className="text-center mt-5 text-uppercase">
+            <p>copyright &copy; all right reserved.Created by Shahzain</p>
+          </div>
+           </div>
+      </footer>
+      
+
     )
 }
--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -9,7 +9,31 @@ export const Team =()=> {
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                 <div className="row">
                     <div className="col-lg-4 col-md-4 col-sm-4 col-10 m-auto d-block">
-                        
+                        <figure className="figure">
+                            <img src={} alt="" className="figure-img img-fluid rounded-circle" height="250px"width="250px"/>
+                            <figcaption>
+                                <h4>Kay Garland</h4>
+                                <p className="figure-caption">Lead Designer</p>
+                            </figcaption>
+                        </figure>
+                    </div>
+                    <div className="col-lg-4 col-md-4 col-sm-4 col-10 m-auto d-block">
+                        <figure className="figure">
+                            <img src={} alt="" className="figure-img img-fluid rounded-circle" height="250px"width="250px"/>
+                            <figcaption>
+                                <h4>Kay Garland</h4>
+                                <p className="figure-caption">Lead Designer</p>
+                            </figcaption>
+                        </figure>
+                    </div>
+                    <div className="col-lg-4 col-md-4 col-sm-4 col-10 m-auto d-block">
+                        <figure className="figure">
+                            <img src={} alt="" className="figure-img img-fluid rounded-circle" height="250px"width="250px"/>
+                            <figcaption>
+                                <h4>Kay Garland</h4>
+                                <p className="figure-caption">Lead Designer</p>
+                            </figcaption>
+                        </figure>
                     </div>
                 </div>
             </div>
