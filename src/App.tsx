2e39408610713cd98a5652ee4f0ad49487b275c8
shahzain <69709547+shahzain-lab@users.noreply.github.com>
2021-01-20 08:46:47 -0800

    team content added

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
      <Header />
      <Services />
      <Portfolio />
-     {/* <Team /> */}
+     <Team />
      <Footer />
     </div>
   );
--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
-
+import team1 from '../img/team1.jpg';
+import team2 from '../img/team2.jpg';
+import team3 from '../img/team3.jpg';
 
 export const Team =()=> {
     return(
@@ -7,10 +9,10 @@ export const Team =()=> {
             <div className="container text-center">
                 <h2>Our Team</h2>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                <div className="row">
+                <div className="row teamrow">
                     <div className="col-lg-4 col-md-4 col-sm-4 col-10 m-auto d-block">
                         <figure className="figure">
-                            <img src={} alt="" className="figure-img img-fluid rounded-circle" height="250px"width="250px"/>
+                            <img src={team1} alt="" className="figure-img img-fluid rounded-circle" height="200px"width="200px"/>
                             <figcaption>
                                 <h4>Kay Garland</h4>
                                 <p className="figure-caption">Lead Designer</p>
@@ -19,19 +21,19 @@ export const Team =()=> {
                     </div>
                     <div className="col-lg-4 col-md-4 col-sm-4 col-10 m-auto d-block">
                         <figure className="figure">
-                            <img src={} alt="" className="figure-img img-fluid rounded-circle" height="250px"width="250px"/>
+                            <img src={team2} alt="" className="figure-img img-fluid rounded-circle" height="250px"width="250px"/>
                             <figcaption>
-                                <h4>Kay Garland</h4>
-                                <p className="figure-caption">Lead Designer</p>
+                                <h4>John Doe</h4>
+                                <p className="figure-caption">CEO</p>
                             </figcaption>
                         </figure>
                     </div>
                     <div className="col-lg-4 col-md-4 col-sm-4 col-10 m-auto d-block">
                         <figure className="figure">
-                            <img src={} alt="" className="figure-img img-fluid rounded-circle" height="250px"width="250px"/>
+                            <img src={team3} alt="" className="figure-img img-fluid rounded-circle" height="250px"width="250px"/>
                             <figcaption>
-                                <h4>Kay Garland</h4>
-                                <p className="figure-caption">Lead Designer</p>
+                                <h4>Smith </h4>
+                                <p className="figure-caption">Share Holder</p>
                             </figcaption>
                         </figure>
                     </div>
